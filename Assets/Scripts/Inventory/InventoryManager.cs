fb6eceeca14dabdf7012f719be0afe969ff8f888
riverhuang-batam <riverhuang011@gmail.com>
2022-02-25 12:55:41 +0700

    initial commit

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class InventoryManager : MonoBehaviour
+{
+    [Header("Inventory Information")]
+    public Inventory playerInventory;
+    [SerializeField] private GameObject blankInventorySlot;
+    [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private GameObject useButton;
+    public InventoryItem currentItem;
+    
+    public void SetTextAndButton(string description, bool buttonActive)
+    {
+        descriptionText.text = description;
+        if (buttonActive)
+        {
+            useButton.SetActive(true);
+        }
+        else
+        {
+            useButton.SetActive(false);
+        }
+    }
+    void MakeInventorySlots()
+    {
+        
+        if (playerInventory)
+        {
+            
+            for (int i = 0; i < playerInventory.myInventory.Count; i++)
+            {
+                if (playerInventory.myInventory[i].numberHeld > 0 || playerInventory.myInventory[i].myName == "bottle")
+                {
+                    
+                    GameObject temp = Instantiate(blankInventorySlot, inventoryPanel.transform.position, Quaternion.identity, inventoryPanel.transform.parent);
+
+                    temp.transform.SetParent(inventoryPanel.transform);
+                    InventorySlot newSlot = temp.GetComponent<InventorySlot>();
+                    if (newSlot)
+                    {
+                        //Debug.Log(playerInventory.myInventory[i]);
+                        newSlot.Setup(playerInventory.myInventory[i], this);
+                    }
+                }
+
+            }
+        }
+    }
+    //void OnEnable()
+    //{
+    //    ClearInventorySlots();
+    //    MakeInventorySlots();
+    //    SetTextAndButton("", false);
+    //}
+
+    // Update is called once per frame
+    void Start()
+    {
+        ClearInventorySlots();
+        MakeInventorySlots();
+        SetTextAndButton("", false);
+    }
+    public void SetupDescriptionAndButton(string newDescriptionString, bool isButtonUsable, InventoryItem newItem)
+    {
+        currentItem = newItem;
+        descriptionText.text = newDescriptionString;
+        useButton.SetActive(isButtonUsable);
+    }
+    public void ClearInventorySlots()
+    {
+        for (int i = 0; i < inventoryPanel.transform.childCount; i++)
+        {
+            Destroy(inventoryPanel.transform.GetChild(i).gameObject);
+        }
+    }
+    public void UseButtonPressed()
+    {
+        if (currentItem)
+        {
+            playerInventory.UseItem(currentItem);
+            ClearInventorySlots();
+            MakeInventorySlots();
+            if (currentItem.numberHeld == 0)
+            {
+                SetTextAndButton("", false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class InventorySlot : MonoBehaviour
+{
+    [Header("")]
+    [SerializeField] private TextMeshProUGUI itemNumberText;
+    [SerializeField] private Image itemImage;
+    [Header("Variables from the item")]
+    //public Sprite itemSprite;
+    //public int numberHeld;
+    //public string itemDescription;
+    public InventoryItem thisItem;
+    public InventoryManager thisManager;
+
+    public void Setup(InventoryItem newItem, InventoryManager newManager)
+    {
+        thisItem = newItem;
+        thisManager = newManager;
+        if (thisItem)
+        {
+            
+            Debug.Log(thisItem.mySprite);
+            if (newItem.mySprite) {
+                itemImage.sprite = thisItem.mySprite;
+                itemNumberText.text = "" + thisItem.numberHeld;
+            }
+            
+        }
+    }
+    public void ClickedOn()
+    {
+        if (thisItem)
+        {
+            thisManager.SetupDescriptionAndButton(thisItem.myDescription, thisItem.isUsable, thisItem);
+        }
+    }
+}
